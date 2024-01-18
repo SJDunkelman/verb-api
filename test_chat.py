d453e25cfe406fbe58ed7a5d2542e24444715fb6
simondunkelman <simon@dunkelman.xyz>
2024-01-18 09:15:24 +0100

    added dotenv

---
+++ test_chat.py
@@ -0,0 +1,13 @@
+# from supabase import Client as SupabaseClient
+# from utils.security import get_user_from_token
+# from dependencies import get_redis, get_db
+# import json
+# from uuid import uuid4
+# from redis import Redis
+#
+#
+# # if __name__ == "__main__":
+# #     # workflow_id =
+# #     message_id = str(uuid4())
+# #     chat_channel = f"workflow:{workflow_id}:chat"
+# #     user = {'id': "249bb8f0-2c31-48c0-ac05-37059f638dc2"}
